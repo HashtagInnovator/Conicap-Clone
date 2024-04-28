2c826ff846a254bf8626c132f3b3c45b25c1242e
Chetan Ohri <chetanohri2000@gmail.com>
2024-04-28 18:33:19 +0530

    Created Coincap Clone

---
+++ src/Components/CurrCyptoInfo/CurrCryptoInfo.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useDispatch, useSelector } from "react-redux";
+import { SingleCryptoInfo } from '../SingleCryptoInfo/SingleCryptoInfo'
+import { fetchCryptoCoins } from '../../features/CryptoSlice/cryptoSlice';
+import "./CurrCryptoInfo.css"
+
+export const CurrCryptoInfo = () => {
+  
+  const dispatch = useDispatch();
+  const {cryptoCoins} = useSelector((state)=> state.cryptoCoins)
+  const isLoading = useSelector((state)=> state.cryptoCoins.isLoading)
+  const [page,setPage] = useState(1)
+
+
+
+  useEffect(()=>{
+      dispatch(fetchCryptoCoins(page))
+  },[page])
+
+
+  return (
+    <>
+    <section className='crypto__container'>
+        <table className='crypto__table'>
+            <thead>
+                <tr className='crypto__header__row'>
+                    <td className='Cypto__Info'>Rank</td>
+                    <td className='Cypto__Info crypto__name'>Name</td>
+                    <td className='Cypto__Info'>Price</td>
+                    <td className='Cypto__Info'>Market Cap</td>
+                    <td className='Cypto__Info'>VWAP (24Hr)	</td>
+                    <td className='Cypto__Info'>Supply</td>
+                    <td className='Cypto__Info'>Volume (24Hr)	</td>
+                    <td className='Cypto__Info'>Change (24Hr)</td>
+                </tr>
+            </thead>
+            <tbody>
+              {cryptoCoins.map((cryptoCoin)=>(
+                <SingleCryptoInfo key={cryptoCoin.id} cryptoCoin={cryptoCoin} />
+              ))}
+            </tbody>
+        </table>
+    </section>
+        <div className='more'>
+        <button onClick={()=>setPage((pre)=>pre+1)} className='action__btn'>{ isLoading ? "loading..." : "View More" }</button>
+        </div>
+    </>
+  )
+}
